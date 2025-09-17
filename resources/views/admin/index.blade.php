82500795cd031106d9d83e0dafab4c843e245278
Ashish Rana <ashishrana288@gmail.com>
2025-09-17 12:47:06 +0530

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <title></title>
+    </head>
+    <body>
+       
+   
+    <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+
+
+    </body>
+</html>
